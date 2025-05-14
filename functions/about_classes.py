575087e50c0df54187c19924483eebab4f7df99d
ian kariuki <iankariuki118@gmail.com>
2025-05-14 12:07:24 +0300

    classes and inheritance

---
+++ functions/about_classes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print (self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+x = Student("mike", "olsen", 2019)
+print(x.graduationyear)
